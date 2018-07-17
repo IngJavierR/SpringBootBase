8c71a2c35a7893e2759ede2c6899bc0a7060bb29
Javier Rodriguez <javierrodriguez@MacBook-Pro-de-usuario.local>
2018-07-17 18:21:30 -0500

    Se cambia ruta de application properties principal

--- project-web/src/main/java/mx/com/axity/web/rest/HelloController.java
+++ project-web/src/main/java/mx/com/axity/web/rest/HelloController.java
@@ -5,6 +5,8 @@ import mx.com.axity.commons.to.UserTO;
 import mx.com.axity.services.facade.ProjectFacade;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,8 +23,9 @@ public class HelloController {
     ProjectFacade projectFacade;
 
     @RequestMapping(value = "/find", method = RequestMethod.GET, produces = "application/json")
-    public List<UserTO> index() {
+    public ResponseEntity<List<UserTO>> index() {
         log.info("Se invoca /users");
-        return this.projectFacade.getAllUsers();
+        var users = this.projectFacade.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
