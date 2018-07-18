58e246376c0b9fd5efaf2aeedd39f1a68d02e44d
Javier Rodriguez <javierrodriguez@MacBook-Pro-de-usuario.local>
2018-07-18 10:34:02 -0500

    Cambios de estructura

--- project-web/src/main/java/mx/com/axity/web/rest/HelloController.java
+++ project-web/src/main/java/mx/com/axity/web/rest/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/users")
+@RequestMapping("users")
 @Api(value="users", description="Operaciones con usuarios")
 public class HelloController {
 
