606ec186437421f8b36e296730aecf6f620bfdd6
IngJavierR <hazelapd@gmail.com>
2018-08-03 12:08:05 -0500

    Se configura CORS

--- project-web/src/main/java/mx/com/axity/web/rest/HelloController.java
+++ project-web/src/main/java/mx/com/axity/web/rest/HelloController.java
@@ -7,11 +7,13 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 @RequestMapping("project")
 @Api(value="project", description="Operaciones con project")
